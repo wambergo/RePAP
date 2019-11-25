a995e60c0c6e70c6a70bbe51e8049fcdfd134da4
wambergo <wamberg@protonmail.com>
2019-11-25 14:09:04 +0100

    modified:   create_densities/create_dens_meansorted.py

--- create_densities/create_dens_meansorted.py
+++ create_densities/create_dens_meansorted.py
@@ -15,8 +15,9 @@ def load_trajectory(filename):
     return atom_objects
 
 def atoms_to_array(atom_objects, n_train):
-    """Take an ASE atom object and convert it into a matrix of size
-    (n_train, n_atoms, 3) such that each row is as atom and the columns 
+    """atoms_to_array
+    Take an ASE atom object and convert it into a matrix of size
+    (n_train, n_atoms, 3) such that each row is as atom and the columns
     are the xyz coordinates
 
     :param atoms: Atoms objects defining atoms position in space
@@ -30,24 +31,25 @@ def atoms_to_array(atom_objects, n_train):
         atoms = atom_objects[i]
         xyz = atoms.get_positions() # get xyz coordinates
         pos.append(xyz)
-        
+
     return pos
 
 def mean_sorting(pos, n_train):
-    """Take position array from the atoms_to_array function, and sort atoms
+    """mean_sorting
+    Take position array from the atoms_to_array function, and sort atoms
     by their mean position to the origin
-    
-    :param pos: (n_train, n_atoms, 3) array obtained from atoms_to_aray 
-    
+
+    :param pos: (n_train, n_atoms, 3) array obtained from atoms_to_array
+
     :return numpy array of dimension (n_train, n_atoms, 3)
     """
-    
+
     mean_xyz = np.mean(pos, axis=0)
     mean_dist = np.linalg.norm(mean_xyz, axis=1)
-    
+
     pos_mean_sort = []
     putting_sequence = []
-    
+
     for i in range(n_train):
         pos_snap_i = pos[i]
         pos_mean_sort = pos_snap_i[mean_dist.argsort()]
@@ -63,6 +65,10 @@ def gaussian_expansion(input_array, n_grid_points, sigma, n_train, n_type):
     of the last dimension.
 
     :param input_array:
+    :n_grid_points: Number of grid points, i.e. resolution of the gaussain expansion
+    :sigma: Kernel parameter for density smoothing
+    :n_train: size of the expanded array
+    :n_type: number of atom types, default = 1
     :return expanded_array
     """
     x_grid = np.linspace(0,10,n_grid_points + 1)
@@ -102,7 +108,7 @@ if __name__ == '__main__':
 
     # Create matrix of size (n_atoms, 3) with the coordinates of each atom
     pos = atoms_to_array(atom_objects, args.n_train)
-    
+
     # sort atoms by atoms mean distance to the origin
     putting_sequence = mean_sorting(pos, args.n_train)
 
