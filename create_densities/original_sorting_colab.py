cecfbc25d584ce3fe22284d9acb774b57b51211c
wambergo <wamberg@protonmail.com>
2020-01-07 20:16:59 +0100

    Update original_sorting_colab.py

--- create_densities/original_sorting_colab.py
+++ create_densities/original_sorting_colab.py
@@ -7,7 +7,6 @@ def original_sorting(pos):
         xyz = pos[i]
         dist = np.linalg.norm(xyz, axis = 1) # compute distance to origin
         pos_sorted = xyz[dist.argsort()] # sort the atoms by their distances
-        print(4)
         putting_sequence.append(pos_sorted)
 
     return putting_sequence
